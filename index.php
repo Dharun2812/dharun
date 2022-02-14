e7b9f1581c485b7254ab33af617da4b7718c9d7d
Dharun2812 <97323354+Dharun2812@users.noreply.github.com>
2022-02-14 23:46:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,76 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
-    <title>ReBook</title>
-    
-    <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="forms.css">
-    
-  </head>
-  
-  <body>
-
-    <header> 
-      <h1>Rebook</h1>
-    </header>
-
-    <nav>
-      <div id="header_menu" class="icon-bar">
-        <a href="index.php">Home</a>
-        <a href="#">Donate</a>
-        <a id="login" style="float:right" type="a" value="LogIn" onclick="document.getElementById('login_box').style.display='block';document.getElementById('login').style.display='none';document.getElementById('body').style.opacity='0.2'">LogIn</a>
-      </div>
-    </nav>
-
-    <section id="body">
-    
-      <div id="Our_Aim" class="aim">
-
-      </div>
-      
-      <div id="book_category" class="category">
-        <button id="btn_jee" class="book_category_style">JEE</button>
-        <button id="btn_neet" class="book_category_style">NEET</button>
-        <button id="btn_cbseicse" class="book_category_style">CBSE</button>
-        <button id="btn_state" class="book_category_style">STATE</button>
-      </div>
-    </section>
-
-    <footer>
-    
-      <div id="contact_us" class="contact">
-        <h2>Contact Us</h2>
-        <a href="mailto:dharunramsarathy@gmail.com">Mail address</a>
-      </div>
-    </footer>
-
-    <div id="login_box" class="box" style="display:none">
-      <span class="close">close &times;</span>
-      <div id="signup_link_div"><label class="form_switching_link">Sign up</label></div>
-      <label id='label0' class='label0'>Log in</label><br>
-      <form action="login.php" method="post">
-          <label class="label1">E-mail</label>
-          <input type="email" name="email" class="input"><br>
-          <label class="label1">Password</label>
-          <input type="password" name="pw" class="input"><br>
-          <br>
-          <input type="submit" id='submit_btn' class='button0' value="Log in">
-      </form>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  
+    <link rel="stylesheet" href="css/forms2.css">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="modal-box">
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary btn-lg show-modal" data-toggle="modal" data-target="#myModal">
+                  Login Form
+                </button>
+ 
+                <!-- Modal -->
+                <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content clearfix">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                            <div class="modal-body">
+                                <h3 class="title">Login Form</h3>
+                                <p class="description">Login here Using Email & Password</p>
+                                <div class="form-group">
+                                    <span class="input-icon"><i class="fa fa-user"></i></span>
+                                    <input type="email" class="form-control" placeholder="Enter email">
+                                </div>
+                                <div class="form-group">
+                                    <span class="input-icon"><i class="fas fa-key"></i></span>
+                                    <input type="password" class="form-control" placeholder="Password">
+                                </div>
+                                <div class="form-group checkbox">
+                                    <input type="checkbox">
+                                    <label>Remamber me</label>
+                                </div>
+                                <a href="" class="forgot-pass">Forgot Password?</a>
+                                <button class="btn">Login</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <script>
-      var closebtns = document.getElementsByClassName("close");
-      var i;
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-      for (i = 0; i < closebtns.length; i++) {
-        closebtns[i].addEventListener("click", function() {
-          this.parentElement.style.display = 'none';
-          window.location.reload();
-        });
-      }
-    </script>
-  </body>
+</body>
 </html>
