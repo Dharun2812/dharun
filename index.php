6197f358deb1bbeb97b07441ac6acadeb36016f3
Dharun2812 <97323354+Dharun2812@users.noreply.github.com>
2022-02-15 12:32:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     
     <title>ReBook</title>
     
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/forms2.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="forms.css">
     <script src="https://code.jquery.com/jquery-3.6.0.js" type="text/javascript"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  
 
